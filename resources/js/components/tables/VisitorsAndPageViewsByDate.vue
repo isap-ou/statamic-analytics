b64fecb7e01088a59387cae8b9bacf28c4987f25
Andrii Trush <andrii.trush@gmail.com>
2026-01-09 19:58:07 +0100

    feat(charts): remove VisitorsAndPageViewsByDate.vue and replace with a new table component for better data representation feat(tables): add VisitorsAndPageViewsByDate.vue to display user engagement data in a table format feat(views): create widget.blade.php to integrate Google Analytics charts into the view feat(google-analytics): implement HasGoogleAnalyticsConfig trait for managing Google Analytics configuration feat(controllers): add GAConfigController to handle Google Analytics settings and updates feat(models): create GoogleAnalyticsConfig model to manage Google Analytics site configurations

---
+++ resources/js/components/tables/VisitorsAndPageViewsByDate.vue
@@ -0,0 +1,53 @@
+<script>
+
+import Card from "../common/Card.vue";
+import fetch from "../../mixins/fetch";
+
+export default {
+  name: "VisitorsAndPageViewsByDate", components: {Table, Card},
+  mixins: [fetch],
+  data() {
+    return {
+
+      sortColumn: 'activeUsers',
+      sortDirection: 'desc',
+      columns: [{
+        'field': 'title',
+        'label': 'Page Title',
+        sortable: true,
+      }, {
+        'field': 'users',
+        'label': 'Users',
+        numeric: true,
+        sortable: true,
+      }, {
+        'field': 'views',
+        'label': 'Views',
+        numeric: true,
+        sortable: true,
+      }, {
+        'field': 'pagesPerUser',
+        'label': 'Pages/User',
+        numeric: true,
+        sortable: true,
+      }, {
+        'field': 'bars',
+        'label': 'Trend',
+      }]
+    }
+  }
+}
+</script>
+
+<template>
+  <Card
+    header="Top Pages (Engagement)"
+    :loading="loading"
+  >
+    <Table v-bind="{columns, data, sortColumn, sortDirection}"/>
+  </Card>
+</template>
+
+<style scoped>
+
+</style>
