88dd4eba53d0bfce36216a32af9dbfc3a7a84a71
Andrii Trush <andrii.trush@gmail.com>
2026-01-12 18:07:12 +0100

    chore(components): add copyright notice to widget components for legal protection and compliance style(TopReferrers.vue): replace HTML comment with block comment for consistency in copyright declaration

--- resources/js/components/widgets/MostVisitedPages.vue
+++ resources/js/components/widgets/MostVisitedPages.vue
@@ -1,4 +1,15 @@
 <script setup>
+
+/*
+ * Copyright (c) 2026 ISAPP (isapp.be)
+ * All rights reserved.
+ *
+ * This source code is proprietary and confidential.
+ * No part of this software may be reproduced, distributed, or transmitted in any form or by any means without prior written permission from ISAPP.
+ *
+ * License: Commercial. See LICENSE.md.
+ */
+
 import AnalyticsWrapper from "../common/AnalyticsWrapper.vue";
 import { computed, shallowRef } from "vue";
 import { useFetch, widgetProps } from "../../composables/fetch.js";
--- resources/js/components/widgets/TopReferrers.vue
+++ resources/js/components/widgets/TopReferrers.vue
@@ -1,14 +1,14 @@
-<!--
-  - Copyright (c) 2026 ISAPP (isapp.be)
-  - All rights reserved.
-  -
-  - This source code is proprietary and confidential.
-  - No part of this software may be reproduced, distributed, or transmitted in any form or by any means without prior written permission from ISAPP.
-  -
-  - License: Commercial. See LICENSE.md.
-  -->
-
 <script setup>
+
+/*
+ * Copyright (c) 2026 ISAPP (isapp.be)
+ * All rights reserved.
+ *
+ * This source code is proprietary and confidential.
+ * No part of this software may be reproduced, distributed, or transmitted in any form or by any means without prior written permission from ISAPP.
+ *
+ * License: Commercial. See LICENSE.md.
+ */
 import AnalyticsWrapper from "../common/AnalyticsWrapper.vue";
 import StatisticTable from "../common/StatisticTable.vue";
 import { useFetch, widgetProps } from "../../composables/fetch";
